e0559fc5d00561d663288729991762eb2d4a618a
Amy Amrani <vesamrani@gmail.com>
2020-08-01 14:47:53 -0400

    prepared for deployment

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const MOVIEDEX = require('./moviedex.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -18,7 +20,6 @@ app.use(function validateBearerToken(req, res, next) {
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
   }
-  // move to the next middleware
   next()
 })
 
@@ -46,7 +47,17 @@ app.get('/movie', function handleGetMovie(req, res) {
   res.json(response)
 })
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
